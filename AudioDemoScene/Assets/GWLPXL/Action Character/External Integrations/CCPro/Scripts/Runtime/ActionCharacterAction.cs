e79dd6ac8ae5a7f36b2aea7441e233b084131f39
Marchusky <marchusky@gmail.com>
2022-05-17 19:36:31 +0200

    MapAdded

---
+++ AudioDemoScene/Assets/GWLPXL/Action Character/External Integrations/CCPro/Scripts/Runtime/ActionCharacterAction.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lightbug.CharacterControllerPro.Implementation;
+namespace GWLPXL.ActionCharacter.Integration.CCPro
+{
+    /// <summary>
+    /// doesnt seem necesssary
+    /// </summary>
+    public class ActionCharacterAction : CharacterState
+    {
+
+       
+
+        public override void UpdateBehaviour(float dt)
+        {
+           
+        }
+
+       
+    }
+}
