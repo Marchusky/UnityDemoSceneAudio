14eedac209d6a3b4006f973f31ac36da11b68603
BooStarGamer <marti.buxeda@gmail.com>
2022-05-18 23:39:39 +0200

    Scene deliverable

---
+++ AudioDemoScene/Assets/Scripts/EnvironmentChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class EnvironmentChange : MonoBehaviour
+{
+
+    public AudioMixerSnapshot changeSnapshot;
+    public AudioMixerSnapshot defaultSnapshot;
+    public string triggerTag;
+
+    public float transitionTime = 0.5f;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag != triggerTag) return;
+        changeSnapshot.TransitionTo(transitionTime);
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag != triggerTag) return;
+        defaultSnapshot.TransitionTo(transitionTime);
+    }
+}
